6ea53738052577d35064a85ec5522f000220439e
yammmt <cu.cs.y20@gmail.com>
2020-04-15 20:58:38 +0900

    :bowtie:

--- exercises/error_handling/errors2.rs
+++ exercises/error_handling/errors2.rs
@@ -16,16 +16,19 @@
 // There are at least two ways to implement this that are both correct-- but
 // one is a lot shorter! Execute `rustlings hint errors2` for hints to both ways.
 
-// I AM NOT DONE
-
 use std::num::ParseIntError;
 
 pub fn total_cost(item_quantity: &str) -> Result<i32, ParseIntError> {
     let processing_fee = 1;
     let cost_per_item = 5;
-    let qty = item_quantity.parse::<i32>();
+    match item_quantity.parse::<i32>() {
+        Ok(qty) => Ok(qty * cost_per_item + processing_fee),
+        Err(sth) => Err(sth)
+    }
 
-    Ok(qty * cost_per_item + processing_fee)
+    // or pass error message with `?`
+    // let qty = item_quantity.parse::<i32>()?;
+    // Ok(qty * cost_per_item + processing_fee)
 }
 
 #[cfg(test)]
@@ -41,7 +44,7 @@ mod tests {
     fn item_quantity_is_an_invalid_number() {
         assert_eq!(
             total_cost("beep boop").unwrap_err().to_string(),
-            "invalid digit found in string"
+            "invalid digit found in string" // default error message
         );
     }
 }
